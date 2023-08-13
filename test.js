1a3bbdf99dcab7449fe39a495a62ab4cfbce7a95
yehudaOhana <yehuda9955@gmail.com>
2023-08-13 15:59:58 +0300

    test2

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const fetchData = async () => {
-  const responsive = await fetch(`https://randomuser.me/api`)
-  .then(data => data.json())
-  .then(users => console.log(users));
+  const responsive = await fetch(`https://randomuser.me/api`);
+  // console.log(responsive);
+  let data = await responsive.json()
+  console.log(data);
 }
 let button = document.getElementById(`button`)
 button.addEventListener(`click`, () => fetchData())
